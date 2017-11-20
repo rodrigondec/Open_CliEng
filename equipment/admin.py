bd79e3c68485048fa5dab77386a0153e218d45d8
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-20 17:26:19 -0300

    minor fix

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Equipment, Contract, Maintenance
+from .models import Equipment, Contract
 
 # Register your models here.
 admin.site.register(Equipment)
 admin.site.register(Contract)
-admin.site.register(Maintenance)
