a6393cb6daa46be429428aa6c75ecb4394749d4d
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-20 01:48:48 -0300

    settings ok

--- Open_Clineng/settings.py
+++ Open_Clineng/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'equipment.apps.EquipmentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Open_Clineng.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Open_Clineng',
+        'USER': 'rodrigondec',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
