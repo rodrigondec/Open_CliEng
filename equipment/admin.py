12cd17592dc35e24d1c2d2e76daf40a5a553799b
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-20 02:15:50 -0300

    manutenção adicionada

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Equipment, Contract
+from .models import Equipment, Contract, Maintenance
 
 # Register your models here.
 admin.site.register(Equipment)
 admin.site.register(Contract)
+admin.site.register(Maintenance)
--- equipment/models.py
+++ equipment/models.py
@@ -3,17 +3,35 @@ from django.db import models
 
 class Equipment(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-
+    description = models.TextField(max_length=200)
     buy_date = models.DateTimeField('date bought')
+    maintenance_freq = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
 
 
+class Maintenance(models.Model):
+    PREVENTIVE = 'PV'
+    OTHER = 'OT'
+
+    TYPE_CHOICES = (
+        (PREVENTIVE, 'Preventiva'),
+        (OTHER, 'Outra'),
+    )
+
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    description = models.TextField(max_length=200)
+    category = models.CharField(max_length=2, choices=TYPE_CHOICES, default=PREVENTIVE)
+    date = models.DateTimeField('date realized')
+
+    def __str__(self):
+        return "Maintenance for {} equipment.".format(self.equipment.name)
+
+
 class Contract(models.Model):
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    text = models.CharField(max_length=800)
+    description = models.TextField(max_length=800)
 
     def __str__(self):
         return "Contract for {} equipment.".format(self.equipment.name)
