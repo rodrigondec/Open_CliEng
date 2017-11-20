c8a9a7712abeabc16f1f02bf927418de9d8fe631
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-20 17:23:08 -0300

    closes #1. Maintenance fora do app equipment

--- equipment/models.py
+++ equipment/models.py
@@ -11,24 +11,6 @@ class Equipment(models.Model):
         return self.name
 
 
-class Maintenance(models.Model):
-    PREVENTIVE = 'PV'
-    OTHER = 'OT'
-
-    TYPE_CHOICES = (
-        (PREVENTIVE, 'Preventiva'),
-        (OTHER, 'Outra'),
-    )
-
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    description = models.TextField(max_length=200)
-    category = models.CharField(max_length=2, choices=TYPE_CHOICES, default=PREVENTIVE)
-    date = models.DateTimeField('date realized')
-
-    def __str__(self):
-        return "Maintenance for {} equipment.".format(self.equipment.name)
-
-
 class Contract(models.Model):
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     description = models.TextField(max_length=800)
