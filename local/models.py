7d206191eafcb83072ae6756ebe9606e12463667
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-21 02:19:43 -0300

    nomes alterados

--- local/models.py
+++ local/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Building(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(max_length=200)
+class Predio(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(max_length=200, null=True)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
-class Sector(models.Model):
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+class Setor(models.Model):
+    predio = models.ForeignKey(Predio, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.nome
