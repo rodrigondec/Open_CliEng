7a6a8312338f58f634ffc03060545bd403389f06
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-21 02:20:13 -0300

    #7. Tipos de manutenção e responsável adicionados

--- manutencao/admin.py
+++ manutencao/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Maintenance
+from .models import TecnicoResponsavel, EmpresaResponsavel, Qualificacao, ManutencaoPreventiva, ManutencaoCalibragem, ManutencaoCorretiva
 
 # Register your models here.
-admin.site.register(Maintenance)
+admin.site.register(Qualificacao)
+admin.site.register(TecnicoResponsavel)
+admin.site.register(EmpresaResponsavel)
+admin.site.register(ManutencaoPreventiva)
+admin.site.register(ManutencaoCalibragem)
+admin.site.register(ManutencaoCorretiva)
+# admin.site.register()
--- manutencao/models.py
+++ manutencao/models.py
@@ -1,37 +1,62 @@
 from django.db import models
-from equipment.models import Equipment
+from equipamento.models import Equipamento
+
+
+class Qualificacao(models.Model):
+    CALIBRAR = 'CA'
+    MANUTENCAO = 'MA'
+
+    TYPE_CHOICES = (
+        (CALIBRAR, "Calibrar"),
+        (MANUTENCAO, "Realizar manutenção"),
+    )
+
+    equipamento = models.ForeignKey(Equipamento, related_name="qualificacoes")
+    tipo = models.CharField(max_length=2, choices=TYPE_CHOICES, default=CALIBRAR)
+
+    class Meta:
+        unique_together = ('equipamento', 'tipo',)
+
+    def __str__(self):
+        return "{} {}".format(self.tipo, self.equipamento.nome)
 
 
 class Responsavel(models.Model):
-    pass
-
-
-class ResponsavelTecnico(Responsavel):
-    pass
-
-
-class ResponsavelEmpresa(Responsavel):
-    pass
-
-
-class Maintenance(models.Model):
-    equipment = models.ForeignKey(Equipment)
-    responsavel = models.OneToOneField(Responsavel)
-    description = models.TextField(max_length=200)
-
-    date = models.DateTimeField('date realized')
+    nome = models.CharField(max_length=200)
+    qualificacoes = models.ManyToManyField(Qualificacao, related_name='qualificados')
 
     def __str__(self):
-        return "Maintenance for {} equipment.".format(self.equipment.name)
+        return self.nome
 
 
-class Preventive(Maintenance):
-    pass
+class TecnicoResponsavel(Responsavel):
+    cpf = models.CharField(max_length=11)
 
 
-class Calibration(Maintenance):
-    pass
+class EmpresaResponsavel(Responsavel):
+    cnpj = models.CharField(max_length=20)
 
 
-class Corrective(Maintenance):
-    pass
+class Manutencao(models.Model):
+    equipamento = models.ForeignKey(Equipamento)
+    responsavel = models.ForeignKey(Responsavel)
+    descricao = models.TextField(max_length=200)
+
+    data_realizada = models.DateTimeField('data realizada', null=True)
+
+class ManutencaoPreventiva(Manutencao):
+    data_agendada = models.DateTimeField('data agendada')
+
+    def __str__(self):
+        return "Preventiva para {} por {}".format(self.equipamento, self.responsavel)
+
+class ManutencaoCalibragem(Manutencao):
+
+    def __str__(self):
+        return "Calibragem para {} por {}".format(self.equipamento, self.responsavel)
+
+
+class ManutencaoCorretiva(Manutencao):
+
+    def __str__(self):
+        return "Corretiva para {} por {}".format(self.equipamento, self.responsavel)
