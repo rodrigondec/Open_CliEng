38d4c86dc74b742ba7c411db40eb1fe295b51678
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-23 16:24:59 -0300

    muntenção agora é bem específico

--- manutencao/admin.py
+++ manutencao/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import TecnicoResponsavel, EmpresaResponsavel, Qualificacao, ManutencaoPreventiva, ManutencaoCalibragem, ManutencaoCorretiva
+from .models import ManutencaoPreventiva, ManutencaoCalibragem, ManutencaoCorretiva
 
 # Register your models here.
-admin.site.register(Qualificacao)
-admin.site.register(TecnicoResponsavel)
-admin.site.register(EmpresaResponsavel)
 admin.site.register(ManutencaoPreventiva)
 admin.site.register(ManutencaoCalibragem)
 admin.site.register(ManutencaoCorretiva)
--- manutencao/models.py
+++ manutencao/models.py
@@ -1,40 +1,6 @@
 from django.db import models
+from core.models import Responsavel
 from equipamento.models import Equipamento
-
-
-class Qualificacao(models.Model):
-    CALIBRAR = 'CA'
-    MANUTENCAO = 'MA'
-
-    TYPE_CHOICES = (
-        (CALIBRAR, "Calibrar"),
-        (MANUTENCAO, "Realizar manutenção"),
-    )
-
-    equipamento = models.ForeignKey(Equipamento, related_name="qualificacoes")
-    tipo = models.CharField(max_length=2, choices=TYPE_CHOICES, default=CALIBRAR)
-
-    class Meta:
-        unique_together = ('equipamento', 'tipo',)
-
-    def __str__(self):
-        return "{} {}".format(self.tipo, self.equipamento.nome)
-
-
-class Responsavel(models.Model):
-    nome = models.CharField(max_length=200)
-    qualificacoes = models.ManyToManyField(Qualificacao, related_name='qualificados')
-
-    def __str__(self):
-        return self.nome
-
-
-class TecnicoResponsavel(Responsavel):
-    cpf = models.CharField(max_length=11)
-
-
-class EmpresaResponsavel(Responsavel):
-    cnpj = models.CharField(max_length=20)
 
 
 class Manutencao(models.Model):
@@ -44,11 +10,13 @@ class Manutencao(models.Model):
 
     data_realizada = models.DateTimeField('data realizada', null=True)
 
+
 class ManutencaoPreventiva(Manutencao):
     data_agendada = models.DateTimeField('data agendada')
 
     def __str__(self):
         return "Preventiva para {} por {}".format(self.equipamento, self.responsavel)
+
 
 class ManutencaoCalibragem(Manutencao):
 
