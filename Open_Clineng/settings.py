7e6c1ad20f3c62cd38158d63d4e1a43e2ed2c940
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-23 16:28:20 -0300

    settings usando arquivo config

--- Open_Clineng/settings.py
+++ Open_Clineng/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from Open_Clineng.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'equipamento.apps.EquipamentoConfig',
-    'funcionario.apps.FuncionarioConfig',
     'local.apps.LocalConfig',
     'manutencao.apps.ManutencaoConfig',
     'django.contrib.admin',
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'Open_Clineng.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Open_Clineng',
-        'USER': 'rodrigondec',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
