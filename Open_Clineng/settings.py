d59c3dc3bb704a9dc49f5b86ff89b820601367db
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-23 16:52:49 -0300

    minor fix

--- Open_Clineng/settings.py
+++ Open_Clineng/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
