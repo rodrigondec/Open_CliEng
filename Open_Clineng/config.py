d357082b0cd6091447bb3506069780920b4d3dfa
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-23 16:30:37 -0300

    delete config

--- Open_Clineng/config.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "ec2-54-235-123-159.compute-1.amazonaws.com"
-DB_PORT = "5432"
-
-DB_NAME = "dafelg3fd81h3m"
-
-DB_USER = "zamkezdjhbexco"
-DB_PASSWORD = "15083a2d7e83bce97f46cffb22cc0aece4d7c0cf70e0800eeeda804e07d13881"
