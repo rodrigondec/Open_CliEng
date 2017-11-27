b48c11bb68c01620454a1fc19bf48f51f35da992
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-27 18:36:56 -0300

    contrato proprio e comodato

--- equipamento/admin.py
+++ equipamento/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Equipamento, Contrato
+from .models import Equipamento, ContratoProprio, ContratoComodato
 
 # Register your models here.
 admin.site.register(Equipamento)
-admin.site.register(Contrato)
+admin.site.register(ContratoProprio)
+admin.site.register(ContratoComodato)
