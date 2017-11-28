bc6ff78819ed2b2bcc4787fe7bd816909b1247d5
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-28 18:09:51 -0300

    DashBoard Criado. Closes #14

--- equipamento/urls.py
+++ equipamento/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^detalhar', views.index, name='index'),
+    url(r'^detalhar/(?P<equipamentoid>\d+)/$', views.detalhar, name='equipamento_detalhes'),
+    url(r'^detalhar_contrato/(?P<contratoid>\d+)/$', views.detalhar_contrato, name='contrato_detalhes'),
 ]
