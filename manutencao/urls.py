d3ea538dd92a8f2dc7b89191a1644ced1ea5a018
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-28 16:40:35 -0300

    index manutenção

--- manutencao/urls.py
+++ manutencao/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^detalhes/(?P<manutencaoid>\d+)/$', views.manutencao_detalhes, name='manutencao_detalhes'),
 ]
