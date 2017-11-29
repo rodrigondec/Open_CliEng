d21dd10448e2ebc768a6802c16593500b14b0ebe
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-29 01:04:16 -0300

    bd local

--- Open_Clineng/config.py
+++ Open_Clineng/config.py
@@ -1,16 +1,16 @@
-DB_HOST = "ec2-54-235-123-159.compute-1.amazonaws.com"
-DB_PORT = "5432"
-
-DB_NAME = "dafelg3fd81h3m"
-
-DB_USER = "zamkezdjhbexco"
-DB_PASSWORD = "15083a2d7e83bce97f46cffb22cc0aece4d7c0cf70e0800eeeda804e07d13881"
+# DB_HOST = "ec2-54-235-123-159.compute-1.amazonaws.com"
+# DB_PORT = "5432"
+#
+# DB_NAME = "dafelg3fd81h3m"
+#
+# DB_USER = "zamkezdjhbexco"
+# DB_PASSWORD = "15083a2d7e83bce97f46cffb22cc0aece4d7c0cf70e0800eeeda804e07d13881"
 
 ## DB LOCAL
-# DB_HOST = "localhost"
-# DB_PORT = ""
-#
-# DB_NAME = "Open_Clineng"
-#
-# DB_USER = "rodrigondec"
-# DB_PASSWORD = ""
+DB_HOST = "localhost"
+DB_PORT = ""
+
+DB_NAME = "Open_Clineng"
+
+DB_USER = "rodrigondec"
+DB_PASSWORD = ""
