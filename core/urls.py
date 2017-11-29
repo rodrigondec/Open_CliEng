54c2b583dce21ff7f25d2ac0f40c53c85dad0e02
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-28 23:04:27 -0300

    urls e views ok

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^detalhar', views.index, name='index'),
+    url(r'^detalhar_funcionario/(?P<funcionarioid>\d+)/$', views.detalhar_funcionario, name='detalhar_funcionario'),
+    url(r'^detalhar_tecnico/(?P<tecnicoid>\d+)/$', views.detalhar_tecnico, name='detalhar_tecnico'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Funcionario, TecnicoResponsavel
+from manutencao.models import Manutencao
+from equipamento.models import Equipamento
 
-# Create your views here.
+
 @login_required
 def index(request):
-    context = {"usuario_autenticado": request.user.is_authenticated()}
+    context = {}
+    context['usuario_autenticado'] = request.user.is_authenticated()
+
+    context['funcionarios'] = Funcionario.objects.all()
+    context['tecnicos'] = TecnicoResponsavel.objects.all()
+    context['manutencoes'] = Manutencao.objects.all()
+    context['equipamentos'] = Equipamento.objects.all()
+
     return render(request, 'core/index.html', context)
+
+
+@login_required
+def detalhar_funcionario(request, funcionarioid):
+    context = {}
+    context['usuario_autenticado'] = request.user.is_authenticated()
+
+    context['funcionario'] = get_object_or_404(Funcionario, pk=funcionarioid)
+
+    return render(request, 'core/detalhar_funcionario.html', context)
+
+
+@login_required
+def detalhar_tecnico(request, tecnicoid):
+    context = {}
+    context['usuario_autenticado'] = request.user.is_authenticated()
+
+    context['tecnico'] = get_object_or_404(TecnicoResponsavel, pk=tecnicoid)
+
+    return render(request, 'core/detalhar_tecnico.html', context)
