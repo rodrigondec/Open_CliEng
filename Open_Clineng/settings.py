4d201c0d8977742e5f95d2db2ed0a2c36d8ab2e6
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-28 23:18:50 -0300

    loggin adicionado

--- Open_Clineng/settings.py
+++ Open_Clineng/settings.py
@@ -146,3 +146,20 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_URL='/login/'
 LOGIN_REDIRECT_URL = '/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
