d79e24f4ffa4492619cfda5fe9f408e886c89156
RodrigoNdeC <rodrigondec@gmail.com>
2017-11-29 00:07:21 -0300

    script atualizado

--- script.py
+++ script.py
@@ -1,10 +1,9 @@
-export DJANGO_SETTINGS_MODULE=Open_Clineng.settings
-
 import django
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Open_Clineng.settings")
 django.setup()
+
 from equipamento.models import Equipamento
 
 a = Equipamento()
 a.num_serie = 0
-
-
