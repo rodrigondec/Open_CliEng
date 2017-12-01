097b882999f874ccce86ab4ce87e0eb880f7e401
RodrigoNdeC <rodrigondec@gmail.com>
2017-12-01 01:59:52 -0300

    full collapse

--- static/js/side_menu.js
+++ static/js/side_menu.js
@@ -1,5 +1,6 @@
-/* off-canvas sidebar toggle */
-$('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+/*Menu-toggle*/
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
+    alert(1);
 });
