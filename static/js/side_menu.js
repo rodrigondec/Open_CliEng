f56ad79b9d621b62daf1e5558e81ab26574db322
RodrigoNdeC <rodrigondec@gmail.com>
2017-12-01 03:20:56 -0300

    sidebars merged

--- static/js/side_menu.js
+++ static/js/side_menu.js
@@ -1,5 +1,11 @@
 /* off-canvas sidebar toggle */
-$('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+//$('[data-toggle=offcanvas]').click(function() {
+//    $('.row-offcanvas').toggleClass('active');
+//    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+//});
+
+/*Menu-toggle*/
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
 });
